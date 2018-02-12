bf2273674627ab6876e6d2a57a5a2e18c3aef3cc
Ernesto Castelan <ernesto.castelan@vincoorbis.com>
2018-02-12 11:30:09 -0600

    Update BootStrap to create demo data only for development environment

--- grails-app/init/json/api/grails/reference/BootStrap.groovy
+++ grails-app/init/json/api/grails/reference/BootStrap.groovy
@@ -3,31 +3,34 @@ package json.api.grails.reference
 import demo.Article
 import demo.Review
 import demo.User
+import grails.util.Environment
 
 class BootStrap {
 
     def init = { servletContext ->
-        if (!User.count()) {
-            new User(username:'mike', fullname:'Michael Wazowski').save(flush:true)
-            new User(username:'sulley', fullname:'James P. Sullivan').save(flush:true)
-            new User(username:'randall', fullname:'Randall Boggs').save(flush:true)
-            assert User.count() == 3
-        }
-        if (!Article.count()) {
-            new Article(author:User.first(),
-                        title:'Lorem ipsum',
-                        content:'Donec pharetra quis tellus vel mattis. Sed non turpis ultrices.').save(flush:true)
-            assert Article.count() == 1
-        }
-        if (!Review.count()) {
-            new Review(author:User.first(),
-                       article:Article.first(),
-                       score:5,
-                       comment:'Malesuada arcu eu.').save(flush:true)
-            new Review(author:User.last(),
-                       article:Article.first(),
-                       score:4).save(flush:true)
-            assert Review.count() == 2
+        if (Environment.current == Environment.DEVELOPMENT) {
+            if (!User.count()) {
+                new User(username:'mike', fullname:'Michael Wazowski').save(flush:true)
+                new User(username:'sulley', fullname:'James P. Sullivan').save(flush:true)
+                new User(username:'randall', fullname:'Randall Boggs').save(flush:true)
+                assert User.count() == 3
+            }
+            if (!Article.count()) {
+                new Article(author:User.first(),
+                            title:'Lorem ipsum',
+                            content:'Donec pharetra quis tellus vel mattis. Sed non turpis ultrices.').save(flush:true)
+                assert Article.count() == 1
+            }
+            if (!Review.count()) {
+                new Review(author:User.first(),
+                           article:Article.first(),
+                           score:5,
+                           comment:'Malesuada arcu eu.').save(flush:true)
+                new Review(author:User.last(),
+                           article:Article.first(),
+                           score:4).save(flush:true)
+                assert Review.count() == 2
+            }
         }
     }
 
