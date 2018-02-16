7dfcf5187df67b90c08a75ba33a4118935bc4659
Ernesto Castelan <ernesto.castelan@vincoorbis.com>
2018-02-15 19:00:02 -0600

    Update Karate config to run some tests with sample data

--- src/integration-test/groovy/demo/KarateRunnerSpec.groovy
+++ src/integration-test/groovy/demo/KarateRunnerSpec.groovy
@@ -10,31 +10,58 @@ import net.masterthought.cucumber.ReportBuilder
 import spock.lang.Specification
 
 @Integration
-@CucumberOptions(tags=['~@ignore'])
 class KarateRunnerSpec extends Specification {
 
     private static final KARATE_REPORT_PATH = 'build/reports/karate'
     private static final KARATE_XML_PATH = 'build/reports/karate/xml'
     private static final KARATE_THREADS = 1
 
-    void "Run Karate Scenarios"() {
-        setup: "Export current port number"
-            System.setProperty('karate.server.port', serverPort.toString())
+    def setup() {
+        System.setProperty('karate.server.port', serverPort.toString())
+    }
+
+    def cleanupSpec() {
+        List<String> jsonPaths = []
+        new File(KARATE_XML_PATH).eachFileMatch(FileType.FILES, ~/.+\.json/) { File file ->
+            jsonPaths << file.absolutePath
+        }
+
+        Configuration config = new Configuration(new File(KARATE_REPORT_PATH), "Karate Tests")
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config)
+        reportBuilder.generateReports()
+    }
+
+    void "Run Karate scenarios with no data"() {
+        when: "Running Karate"
+            KarateStats stats = CucumberRunner.parallel(KarateNoDataTestSet.class, KARATE_THREADS, KARATE_XML_PATH)
+
+        then: "No errors are expected"
+            stats.failCount == 0
+    }
+
+    void "Run Karate scenarios with test data"() {
+        setup: "Create test users"
+            List<String> usernameList = ['alfa', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot']
+            User.withNewSession {
+                usernameList.each { String username ->
+                    new User(username:username, fullname:"$username zulu").save(flush:true)
+                }
+                assert User.count() == usernameList.size()
+            }
 
         when: "Running Karate"
-            KarateStats stats = CucumberRunner.parallel(getClass(), KARATE_THREADS, KARATE_XML_PATH)
+            KarateStats stats = CucumberRunner.parallel(KarateWithDataTestSet.class, KARATE_THREADS, KARATE_XML_PATH)
 
         then: "No errors are expected"
             stats.failCount == 0
 
-        cleanup: "Generate report"
-            List<String> jsonPaths = []
-            new File(KARATE_XML_PATH).eachFileMatch(FileType.FILES, ~/.+\.json/) { File file ->
-                jsonPaths << file.absolutePath
-            }
-
-            Configuration config = new Configuration(new File(KARATE_REPORT_PATH), "Karate Tests")
-            ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config)
-            reportBuilder.generateReports()
+        cleanup: "Remove test data"
+            User.withNewSession { User.executeUpdate('delete from User') }
     }
 }
+
+@CucumberOptions(tags=['~@ignore', '@nodata'])
+class KarateNoDataTestSet { }
+
+@CucumberOptions(tags=['~@ignore','~@nodata'])
+class KarateWithDataTestSet { }
